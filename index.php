a822beae6ed6406c9d3fa1329bf8428537df2ccd
Stanislav Atanasov <lucifer@anavaro.com>
2015-12-10 05:30:25 +0200

    Remove card height Make Last posts from 5 to 4 add CHANGELOG.txt

--- index.php
+++ index.php
@@ -150,7 +150,7 @@
 	* Get last 4 posts so we cen fill the fields
 	*/
 	$args = array(
-		'numberposts' => 5,
+		'numberposts' => 4,
 		'orderby' => 'post_date',
 		'order' => 'DESC',
 		'post_type' => 'post',
@@ -162,87 +162,75 @@
 	<div class="row" id="latest">
 		<p class="text-center"><?php if (get_theme_mod('posts_page')) { echo '<a href="' . get_permalink(get_theme_mod('posts_page')) . '">'; } ?>Latest blog posts ... <?php if (get_theme_mod('posts_page')) { echo '</a>'; } ?></p>
 	</div>
-	<div class="row" id="posts">
-		<div class="col-sm-1 col-lg-1"></div>
-		<div class="col-sm-5 col-md-3 col-lg-2 card">
-			<a href="<?php echo get_permalink($recent_posts[0]['ID']) ?>">
-			<?php
-				if (has_post_thumbnail( $recent_posts[0]['ID'] ))
-				{
-					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent_posts[0]['ID'] ), 'single-post-thumbnail' );
-					?>
-						<img src="<?php echo $image[0]?>" class="image-responsive"/>
+	<div class="row center-block">
+		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+			<div class="row" id="posts">
+				<div class="col-sm-6 col-md-3 col-lg-3">
+					<a href="<?php echo get_permalink($recent_posts[0]['ID']) ?>">
 					<?php
-					unset($image);
-				}
-			?>
-			<span class="title"><?php echo $recent_posts[0]['post_title'] ?></span>
-			</a>
-			<span class="excerpt"><p><?php echo $recent_posts[0]['post_excerpt'] ?></span>
-		</div>
-		<div class="col-sm-5 col-md-3 col-lg-2 card">
-			<a href="<?php echo get_permalink($recent_posts[1]['ID']) ?>">
-			<?php
-				if (has_post_thumbnail( $recent_posts[1]['ID'] ))
-				{
-					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent_posts[1]['ID'] ), 'single-post-thumbnail' );
+						if (has_post_thumbnail( $recent_posts[0]['ID'] ))
+						{
+							$image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent_posts[0]['ID'] ), 'single-post-thumbnail' );
+							?>
+								<img src="<?php echo $image[0]?>" class="image-responsive"/>
+							<?php
+							unset($image);
+						}
 					?>
-						<img src="<?php echo $image[0]?>" class="image-responsive"/>
+					<span class="title"><?php echo $recent_posts[0]['post_title'] ?></span>
+					</a>
+					<span class="excerpt"><p><?php echo $recent_posts[0]['post_excerpt'] ?></span>
+				</div>
+				<div class="col-sm-6 col-md-3 col-lg-3">
+					<a href="<?php echo get_permalink($recent_posts[1]['ID']) ?>">
 					<?php
-					unset($image);
-				}
-			?>
-			<span class="title"><?php echo $recent_posts[1]['post_title'] ?></span>
-			</a>
-			<span class="excerpt"><p><?php echo $recent_posts[1]['post_excerpt'] ?></span>
-		</div>
-		<div class="col-sm-5 col-sm-offset-1 col-md-3 col-md-offset-0 col-lg-2 col-lg-offset-0 card ">
-			<a href="<?php echo get_permalink($recent_posts[2]['ID']) ?>">
-			<?php
-				if (has_post_thumbnail( $recent_posts[2]['ID'] ))
-				{
-					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent_posts[2]['ID'] ), 'single-post-thumbnail' );
+						if (has_post_thumbnail( $recent_posts[1]['ID'] ))
+						{
+							$image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent_posts[1]['ID'] ), 'single-post-thumbnail' );
+							?>
+								<img src="<?php echo $image[0]?>" class="image-responsive"/>
+							<?php
+							unset($image);
+						}
 					?>
-						<img src="<?php echo $image[0]?>" class="image-responsive"/>
+					<span class="title"><?php echo $recent_posts[1]['post_title'] ?></span>
+					</a>
+					<span class="excerpt"><p><?php echo $recent_posts[1]['post_excerpt'] ?></span>
+				</div>
+				<div class="col-sm-6 col-md-3 col-lg-3">
+					<a href="<?php echo get_permalink($recent_posts[2]['ID']) ?>">
 					<?php
-					unset($image);
-				}
-			?>
-			<span class="title"><?php echo $recent_posts[2]['post_title'] ?></span>
-			</a>
-			<span class="excerpt"><p><?php echo $recent_posts[2]['post_excerpt'] ?></span>
-		</div>
-		<div class="col-sm-5 col-md-3 col-md-offset-1 col-lg-2 col-lg-offset-0 card">
-			<a href="<?php echo get_permalink($recent_posts[3]['ID']) ?>">
-			<?php
-				if (has_post_thumbnail( $recent_posts[3]['ID'] ))
-				{
-					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent_posts[3]['ID'] ), 'single-post-thumbnail' );
+						if (has_post_thumbnail( $recent_posts[2]['ID'] ))
+						{
+							$image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent_posts[2]['ID'] ), 'single-post-thumbnail' );
+							?>
+								<img src="<?php echo $image[0]?>" class="image-responsive"/>
+							<?php
+							unset($image);
+						}
 					?>
-						<img src="<?php echo $image[0]?>" class="image-responsive"/>
+					<span class="title"><?php echo $recent_posts[2]['post_title'] ?></span>
+					</a>
+					<span class="excerpt"><p><?php echo $recent_posts[2]['post_excerpt'] ?></span>
+				</div>
+				<div class="col-sm-6 col-md-3 col-lg-3">
+					<a href="<?php echo get_permalink($recent_posts[3]['ID']) ?>">
 					<?php
-					unset($image);
-				}
-			?>
-			<span class="title"><?php echo $recent_posts[3]['post_title'] ?></span>
-			</a>
-			<span class="excerpt"><p><?php echo $recent_posts[3]['post_excerpt'] ?></span>
-		</div>
-		<div class="col-sm-5 col-sm-offset-1 col-md-3  col-md-offset-0 col-lg-2 col-lg-offset-0 card">
-			<a href="<?php echo get_permalink($recent_posts[4]['ID']) ?>">
-			<?php
-				if (has_post_thumbnail( $recent_posts[4]['ID'] ))
-				{
-					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent_posts[4]['ID'] ), 'single-post-thumbnail' );
+						if (has_post_thumbnail( $recent_posts[3]['ID'] ))
+						{
+							$image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent_posts[3]['ID'] ), 'single-post-thumbnail' );
+							?>
+								<img src="<?php echo $image[0]?>" class="image-responsive"/>
+							<?php
+							unset($image);
+						}
 					?>
-						<img src="<?php echo $image[0]?>" class="image-responsive"/>
-					<?php
-					unset($image);
-				}
-			?>
-			<span class="title"><?php echo $recent_posts[4]['post_title'] ?></span>
-			</a>
-			<span class="excerpt"><p><?php echo $recent_posts[4]['post_excerpt'] ?></span>
+					<span class="title"><?php echo $recent_posts[3]['post_title'] ?></span>
+					</a>
+					<span class="excerpt"><p><?php echo $recent_posts[3]['post_excerpt'] ?></span>
+				</div>
+			</div>
 		</div>
 	</div>
 <div class="splitter"></div>
